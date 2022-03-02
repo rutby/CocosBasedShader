f7c8f97c31e4a74a6df5bb2c64bf4b7c470393f8
chentao <chentao@moqipobing.com>
2022-03-02 13:39:07 +0800

    feat: 补充自定义图片与文字合批 - 追加passes区分

--- assets/Script/Tools/SLBatch/SLBatchAdapter.ts
+++ assets/Script/Tools/SLBatch/SLBatchAdapter.ts
@@ -6,8 +6,10 @@ export default class SLBatchAdapter extends cc.Component {
     @property(cc.Material) material: cc.Material = null;
     
     isCalled: boolean = false;
+    defaultPassHash: string = '';
     
     protected start(): void {
+        this.defaultPassHash = this.getMaterialHash();
         this.generateShareAtlas();
         this.bindShareAtlas();
         this.replaceDefaultMaterial(this.node);
@@ -30,7 +32,7 @@ export default class SLBatchAdapter extends cc.Component {
             // 名称含有noslbatch的节点不做处理
             if (element.name.indexOf('noslbatch') < 0) {
                 var sp = element.getComponent(cc.Sprite);
-                if (sp) {
+                if (sp && this.getRenderComponentHash(sp) == this.defaultPassHash) {
                     var texture = sp.spriteFrame.getTexture();
                     for(var i = 0; i < 8; i++) {
                         //@ts-ignore 
@@ -46,7 +48,7 @@ export default class SLBatchAdapter extends cc.Component {
                 }
                 
                 var label = element.getComponent(cc.Label);
-                if (label) {
+                if (label && this.getRenderComponentHash(label) == this.defaultPassHash) {
                     label.cacheMode = cc.Label.CacheMode.CHAR;
                     label.texId = 1;
                     
@@ -58,5 +60,49 @@ export default class SLBatchAdapter extends cc.Component {
                 this.replaceDefaultMaterial(element);
             }
         })
+    }
+    
+    serializePass(pass) {
+        let str = pass._cullMode;
+        if (pass._blend) {
+            str += pass._blendEq + pass._blendAlphaEq + pass._blendSrc + pass._blendDst
+                + pass._blendSrcAlpha + pass._blendDstAlpha + pass._blendColor;
+        }
+        if (pass._depthTest) {
+            str += pass._depthWrite + pass._depthFunc;
+        }
+        if (pass._stencilTest) {
+            str += pass._stencilFuncFront + pass._stencilRefFront + pass._stencilMaskFront
+                + pass._stencilFailOpFront + pass._stencilZFailOpFront + pass._stencilZPassOpFront
+                + pass._stencilWriteMaskFront
+                + pass._stencilFuncBack + pass._stencilRefBack + pass._stencilMaskBack
+                + pass._stencilFailOpBack + pass._stencilZFailOpBack + pass._stencilZPassOpBack
+                + pass._stencilWriteMaskBack;
+        }
+    
+        return str;
+    }
+    
+    serializePasses (passes) {
+        let hashData = '';
+        for (let i = 0; i < passes.length; i++) {
+            hashData += this.serializePass(passes[i]);
+        }
+        return hashData;
+    }
+    
+    getHash(effectVar) {
+        let hash = '';
+        hash += this.serializePasses(effectVar._passes);
+        
+        return hash;
+    }
+    
+    getRenderComponentHash(rc) {
+        return this.getHash(rc.getMaterials()[0]._effect)
+    }
+    
+    getMaterialHash() {
+        return this.getHash(this.material._effect._technique);
     }
 }
